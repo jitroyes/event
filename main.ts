cf41ee511ee493b97c2d741605fcb396a403f10d
Hugues GUILLEUS <ghugues@netc.fr>
2026-05-19 17:11:52 +0200

    use TOML and add petition

--- main.ts
+++ main.ts
@@ -1,56 +1,89 @@
-import ev2026 from "./2026.json" with { type: "json" };
-import { html, htmlAttr } from "@huguesguilleus/blogger/html";
+import { HTML, html, htmlAttr } from "@huguesguilleus/blogger/html";
+import * as TOML from "@std/toml";
+
+type Data = {
+	petition: {
+		title: string;
+		notes: string[];
+	}[];
+	event: {
+		title: string;
+		date: Date;
+		location: string;
+		notes: string[];
+	}[];
+};
+
+const data = TOML.parse(await Deno.readTextFile("now.toml")) as Data;
 
 export function minify(data: string): string {
-  return data.replaceAll(/(\/\*[\s\S]*?\*\/|\s+)/g, " ")
-    .replaceAll(/([\W^-]) /g, "$1")
-    .replaceAll(/ ([\W]^-)/g, "$1");
+	return data.replaceAll(/(\/\*[\s\S]*?\*\/|\s+)/g, " ")
+		.replaceAll(/([\W^-]) /g, "$1")
+		.replaceAll(/ ([\W]^-)/g, "$1");
 }
 const HEAD = minify(await Deno.readTextFile("head.html"));
 
 await Deno.mkdir("public", { recursive: true });
 await Deno.writeTextFile(
-  "public/index.html",
-  HEAD + html(
-    "body.withgrid",
-    html(
-      "header",
-      html("h1", "Actions dans l'Aube"),
-      html("i", "Généré le ", fmtDate()),
-    ),
-    // // //
-    !!ev2026.length && [
-      html("h2", "Évènements"),
-      html(
-        `div.blocks`,
-        ev2026.filter((event) => event.title).map((event) =>
-          html(
-            "div.bl.event",
-            html("div.type", "// évènement //"),
-            html("h3", event.title),
-            !!event.date && html("div.bold", fmtDate(event.date)),
-            !!event.location && html("div.", event.location),
-            ...(event.notes || []).map((note) =>
-              /^https?:\/\//.test(note)
-                ? htmlAttr`a href='${note}'`(note)
-                : html("p", note)
-            ),
-          )
-        ),
-      ),
-    ],
-    //
-    false && [
-      html("h2", "Pétition"),
-    ],
-  ).h,
+	"public/index.html",
+	HEAD + html(
+		"body.withgrid",
+		html(
+			"header",
+			html("h1", "Actions dans l'Aube"),
+			html("i", "Généré le ", fmtDate()),
+		),
+		// // //
+		!!data.event.length && [
+			html("h2", "Les évènements"),
+			html(
+				"div.blocks",
+				data.event.filter((event) => event.title).map((event) =>
+					html(
+						"div.bl.event",
+						html("div.type", "// évènement //"),
+						html("h3", event.title),
+						!!event.date && html("div.bold", fmtDate(event.date)),
+						!!event.location && html("div", event.location),
+						// ...(event.notes || []).map((note) =>
+						// 	/^https?:\/\//.test(note)
+						// 		? htmlAttr`a href='${note}'`(note)
+						// 		: html("p", note)
+						// ),
+						notes(event.notes),
+					)
+				),
+			),
+		],
+		// // //
+		!!data.petition.length && [
+			html("h2", "Les pétitions"),
+			html(
+				"div.blocks",
+				data.petition.map((petition) =>
+					html(
+						"div.bl.petition",
+						html("h3", petition.title),
+						notes(petition.notes),
+						html(`a href='${petition.url}'`, petition.url),
+					)
+				),
+			),
+		],
+	).h,
 );
 
-// https://eci.ec.europa.eu/055/public/?lg=fr
+function fmtDate(date = new Date()): string {
+	return Intl.DateTimeFormat("fr", {
+		dateStyle: "full",
+		timeStyle: "medium",
+	}).format(date);
+}
 
-function fmtDate(str: string = Date()): string {
-  return Intl.DateTimeFormat("fr", {
-    dateStyle: "full",
-    timeStyle: "medium",
-  }).format(new Date(str));
+function notes(lines: string[] = []): HTML {
+	return lines.map((line) =>
+		/^https?:\/\//.test(line)
+			? htmlAttr`a href='${line}'`(line)
+			: html("p", line)
+	);
 }
