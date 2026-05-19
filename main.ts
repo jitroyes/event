178b6cf8fef74a01dea4e54a313553a6c748c80c
Hugues GUILLEUS <ghugues@netc.fr>
2026-05-19 16:36:36 +0200

    Clean code

--- main.ts
+++ main.ts
@@ -1,11 +1,16 @@
 import ev2026 from "./2026.json" with { type: "json" };
-import { html, minify } from "./html.ts";
+import { html, htmlAttr } from "@huguesguilleus/blogger/html";
 
+export function minify(data: string): string {
+  return data.replaceAll(/(\/\*[\s\S]*?\*\/|\s+)/g, " ")
+    .replaceAll(/([\W^-]) /g, "$1")
+    .replaceAll(/ ([\W]^-)/g, "$1");
+}
 const HEAD = minify(await Deno.readTextFile("head.html"));
 
 await Deno.mkdir("public", { recursive: true });
 await Deno.writeTextFile(
-  "public/2026.html",
+  "public/index.html",
   HEAD + html(
     "body.withgrid",
     html(
@@ -13,25 +18,31 @@ await Deno.writeTextFile(
       html("h1", "Actions dans l'Aube"),
       html("i", "Généré le ", fmtDate()),
     ),
-    !!ev2026.length && html("h2", "Actions"),
-    html("div.grid2"),
-    html(
-      `div.grid2`,
-      ev2026.filter((event) => event.title).map((event) =>
-        html(
-          "div.bl",
-          html("div.type", "// évènement //"),
-          html("h3", event.title),
-          !!event.date && html("div.bold", fmtDate(event.date)),
-          !!event.location && html("div.bold", event.location),
-          ...(event.notes || []).map((note) =>
-            /^https?:\/\//.test(note)
-              ? { h: `<div><a href='${note}'>${note}</a></div>` }
-              : html("p", note)
-          ),
-        )
+    // // //
+    !!ev2026.length && [
+      html("h2", "Évènements"),
+      html(
+        `div.blocks`,
+        ev2026.filter((event) => event.title).map((event) =>
+          html(
+            "div.bl.event",
+            html("div.type", "// évènement //"),
+            html("h3", event.title),
+            !!event.date && html("div.bold", fmtDate(event.date)),
+            !!event.location && html("div.", event.location),
+            ...(event.notes || []).map((note) =>
+              /^https?:\/\//.test(note)
+                ? htmlAttr`a href='${note}'`(note)
+                : html("p", note)
+            ),
+          )
+        ),
       ),
-    ),
+    ],
+    //
+    false && [
+      html("h2", "Pétition"),
+    ],
   ).h,
 );
 
